d61fed0bd01a87189113c8a249839406fed795fb
Timo Varila <timo.varila@myy.haaga-helia.fi>
2019-02-04 21:30:18 +0200

    demo v1

---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="product-list-one">
+    <h1>Viestejä Koa backendiltä...</h1>
+
+    <p>Greeting: {{userGreeting}}</p>
+    <button v-on:click="setGreeting">New Greeting</button>
+    
+    <p>Greeting: {{finGreeting}}</p>
+    <button v-on:click="setFinGreeting">New Greeting</button>
+    
+    <p>Greeting: {{engGreeting}}</p>
+    <button v-on:click="setEngGreeting">New Greeting</button>
+    
+    <p>Greeting: {{sweGreeting}}</p>
+    <button v-on:click="setSweGreeting">New Greeting</button>
+    
+    <p>Greeting: {{gerGreeting}}</p>
+    <button v-on:click="setGerGreeting">New Greeting</button>
+    
+
+    <h2>Product List One</h2>
+    <ul>
+      <li v-for="product in saleProducts">
+        <span class="name">{{ product.name }}</span>
+        <span class="price">€{{ product.price }}</span>
+      </li>
+    </ul>
+    <button v-on:click="reducePrice(1)">Reduce Price</button>
+
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+import {mapGetters} from 'vuex'; 
+
+export default {
+  //props: ['products'],
+  /*
+  data() {
+    return {
+      
+    }
+  }
+  */
+  
+  computed: {
+    products(){
+      return this.$store.state.products;
+    },
+    ...mapGetters([
+      'saleProducts',
+      'userGreeting',
+      'finGreeting',
+      'sweGreeting',
+      'engGreeting',
+      'gerGreeting'
+    ])
+    /*
+    saleProducts(){
+      return this.$store.getters.saleProducts;
+    }
+    */
+  },
+  methods: {
+    ...mapActions([
+      'reducePrice',
+      'setGreeting',
+      'setFinGreeting',
+      'setSweGreeting',
+      'setEngGreeting',
+      'setGerGreeting',
+    ])
+    
+    // reducePrice: function(amount) {
+      /*this.$store.state.products.forEach(element => {
+        element.price  -= 1;
+      });*/
+      /*  this.$store.commit('reducePrice'); */
+    //  this.$store.dispatch('reducePrice', amount)
+    //}
+  }
+}
+</script>
+
+<style scoped>
+ body {
+    font-family: Ubuntu;
+  }
+
+</style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,107 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export const store = new Vuex.Store({
   state: {
+    products: [
+      {name: 'Banana Skin', price: 20},
+      {name: 'Shiny Star', price: 40},
+      {name: 'Green Shells', price: 60},
+      {name: 'Red Shells', price: 80}
+    ],
+    greeting: 'Moi maailma!',
+    finGreeting: 'Hei!',
+    sweGreeting: 'Hej!',
+    engGreeting: 'Hi!',
+    gerGreeting: 'Hallo!',
+  },
 
+  getters: {
+    saleProducts: state => {
+      var saleProducts = state.products.map( product => {
+        return {
+          name: '**' + product.name + '**',
+          price: product.price/2
+        }
+      });
+      return saleProducts;
+
+    },
+    userGreeting: state => { 
+      return state.greeting; },
+    finGreeting: state => { 
+      return state.finGreeting; },
+    sweGreeting: state => { 
+      return state.sweGreeting; },
+    engGreeting: state => { 
+      return state.engGreeting; },
+    gerGreeting: state => { 
+      return state.gerGreeting; }
   },
   mutations: {
-
+    reducePrice: (state, payload) => {
+      state.products.forEach(element => {
+        element.price  -= payload;
+      })
+    },
+    setGreeting: (state, newGreeting) => {
+      state.greeting = newGreeting
+    },
+    setFinGreeting: (state, newGreeting) => {
+      state.finGreeting = newGreeting
+    },
+    setSweGreeting: (state, newGreeting) => {
+      state.sweGreeting = newGreeting
+    },
+    setEngGreeting: (state, newGreeting) => {
+      state.engGreeting = newGreeting
+    },
+    setGerGreeting: (state, newGreeting) => {
+      state.gerGreeting = newGreeting
+    }
   },
   actions: {
+    reducePrice: (context, payload) => {
+      context.commit('reducePrice', payload)
+    },
+    setGreeting: (context) => {
+      axios.get('http://localhost:3000/')
+        .then(response => {
+          console.log(response);
+          context.commit('setGreeting', response.data.msg);
+        });
+    },
+    setFinGreeting: (context) => {
+      axios.get('http://localhost:3000/fin')
+        .then(response => {
+          console.log(response);
+          context.commit('setFinGreeting', response.data.msg);
+        });
+    },
+    setSweGreeting: (context) => {
+      axios.get('http://localhost:3000/swe')
+        .then(response => {
+          console.log(response);
+          context.commit('setSweGreeting', response.data.msg);
+        });
+    },
+    setEngGreeting: (context) => {
+      axios.get('http://localhost:3000/eng')
+        .then(response => {
+          console.log(response);
+          context.commit('setEngGreeting', response.data.msg);
+        });
+    },
+    setGerGreeting: (context) => {
+      axios.get('http://localhost:3000/ger')
+        .then(response => {
+          console.log(response);
+          context.commit('setGerGreeting', response.data.msg);
+        });
+    },
 
   }
 })
